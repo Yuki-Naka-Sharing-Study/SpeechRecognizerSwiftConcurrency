f099e033f99683f6c316e6a7473d2efb8c188f67
仲優樹 <yu-ki216m@outlook.jp>
2025-06-28 10:17:52 +0900

    Initial Commit

---
+++ SpeechRecognizerSwiftConcurrency/SpeechRecognizerSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognizerSwiftConcurrencyApp.swift
+//  SpeechRecognizerSwiftConcurrency
+//
+//  Created by 仲優樹 on 2025/06/28.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognizerSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
